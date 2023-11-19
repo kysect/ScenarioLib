6089c84986af79c3cdfa3944bb57a5c2e9e99d3f
Fredi Kats <fredikats@outlook.com>
2023-11-19 18:11:13 +0100

    Add scenario source code yaml parser

---
+++ Sources/Kysect.ScenarioLib.YamlParser/YamlDeserializerBuilderFactory.cs
@@ -0,0 +1,13 @@
+﻿using YamlDotNet.Serialization;
+
+namespace Kysect.ScenarioLib.YamlParser;
+
+public class YamlDeserializerBuilderFactory
+{
+    public static IDeserializer Create()
+    {
+        return new DeserializerBuilder()
+            .WithNodeDeserializer(new KeyValuePairNodeDeserializer())
+            .Build();
+    }
+}
